d6bb6a593dcc8540b80bdd0bc1aa7fdcc49df859
ALAN DE MEDEIROS <alan_de-medeiros@estudante.sesisenai.org.br>
2024-10-15 21:07:45 -0300

    Avaliacao02

--- src/script.js
+++ src/script.js
@@ -1,1 +1,24 @@
-alert("Script conectado a página")
+document.getElementById('cadastrar').addEventListener('click', function(event) {
+    event.preventDefault(); 
+
+    const marca = document.getElementById('carro-marca').value;
+    const modelo = document.getElementById('carro-modelo').value;
+    const ano = document.getElementById('carro-ano').value;
+    const placa = document.getElementById('carro-placa').value;
+    
+    const img = document.createElement('img');
+                img.src = 'https://st.depositphotos.com/1163717/3353/i/450/depositphotos_33536719-stock-photo-yellow-sports-car.jpg'
+                img.alt = ''
+    
+
+
+    const li = document.createElement('li');
+    li.textContent = `${marca} ${modelo} (${ano}) - Placa: ${placa}`
+    li.appendChild(img);
+    
+    
+    document.getElementById('lista-veiculo').appendChild(li);
+
+    
+    document.getElementById('div-carro').reset();
+});
